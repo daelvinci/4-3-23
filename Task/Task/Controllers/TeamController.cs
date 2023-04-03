3ef320569b89688e29e68d358f35886f5a9b49d5
daelvinci <elvinab@code.edu.az>
2023-04-04 03:03:16 +0400

    added

---
+++ Task/Task/Controllers/TeamController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Task.Models;
+
+namespace Task.Controllers
+{
+    public class TeamController : Controller
+    {
+        private readonly DataContext _context;
+
+        public TeamController(DataContext context)
+        {
+            _context = context;
+            
+        }
+        public ActionResult Index()
+        {
+            var data = _context.Members.ToList();
+            return View(data);
+        }
+        public ActionResult Detail(int id)
+        {
+            var member = _context.Members.Find(id);
+            return View(member);
+        }
+
+
+    }
+}
---
+++ Task/Task/Migrations/20230403222319_TeamTableAdded.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Task.Migrations
+{
+    public partial class TeamTableAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Members",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false).Annotation("SqlServer:Identity", "1, 1"),
+                    BgImg = table.Column<string>(nullable:true),
+                    Icon1=table.Column<string>(nullable:true),
+                    Icon2=table.Column<string>(nullable:true),
+                    Icon3=table.Column<string>(nullable:true),
+                    Icon4=table.Column<string>(nullable:true),
+                    Name=table.Column<string>(nullable:true),
+                    Position=table.Column<string>(nullable:true)
+                },
+
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Members", x => x.Id);
+                }) ;
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name:"Members"
+                );
+        }
+    }
+}
